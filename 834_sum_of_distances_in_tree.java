f9192d52ed1646304cb990d92217c6f5e1df6846
farushed <farushed@tcd.ie>
2022-12-22 22:15:48 +0000

    834

--- 834_sum_of_distances_in_tree.java
+++ 834_sum_of_distances_in_tree.java
@@ -1,64 +1,68 @@
 // https://leetcode.com/problems/sum-of-distances-in-tree/
-// Runtime 290 ms Beats 8.13%
-// Memory 85.3 MB Beats 68.90%
+// Runtime 41 ms Beats 96.65%
+// Memory 61.5 MB Beats 91.87%
+
+class Node {
+    int num;
+    List<Node> children;
+    int sumBelow = 0;
+    int count = 0;
+
+    public Node(int num) {
+        this.num = num;
+        this.children = new ArrayList();
+    }
+}
 
 class Solution {
-    Map<Integer, Integer>[] dfsMemo;
-    Map<Integer, Integer>[] countMemo;
-
     public int[] sumOfDistancesInTree(int n, int[][] edges) {
-        List<Integer>[] graph = new List[n];
-        dfsMemo = new Map[n];
-        countMemo = new Map[n];
-
-        for (int i=0; i<n; i++) {
-            graph[i] = new ArrayList();
-            dfsMemo[i] = new HashMap();
-            countMemo[i] = new HashMap();
-        }
+        Node[] nodes = new Node[n];
+        for (int i=0; i<n; i++)
+            nodes[i] = new Node(i);
 
         for (int[] edge : edges) {
-            graph[edge[0]].add(edge[1]);
-            graph[edge[1]].add(edge[0]);
+            nodes[edge[0]].children.add(nodes[edge[1]]);
+            nodes[edge[1]].children.add(nodes[edge[0]]);
         }
+        removeBackReferences(nodes[0]);
 
+        countAndSum(nodes[0]);
 
         int[] answer = new int[n];
-
-        for (int i=0; i<n; i++) {
-            answer[i] = dfs(-1, i, graph);
+        int sum = nodes[0].sumBelow;
+        answer[0] = sum;
+        for (Node c : nodes[0].children) {
+            dfs(c, sum, n, answer);
         }
-            
+
         return answer;
     }
 
-    private int dfs(int from, int cur, List<Integer>[] graph) {
-        if (dfsMemo[cur].containsKey(from))
-            return dfsMemo[cur].get(from);
+    private void dfs(Node root, int parentSum, int totalCount, int[] answer) {
+        // answer[root.n] = root.sumBelow + (parentSum - (root.sumBelow + root.count) + n - root.count)
+        int sum = parentSum - 2*root.count + totalCount;
+        answer[root.num] = sum;
 
-        int result = 0;
-
-        for (int nei : graph[cur]) {
-            if (nei != from)
-                result += count(cur, nei, graph) + dfs(cur, nei, graph);
+        for (Node c : root.children) {
+            dfs(c, sum, totalCount, answer);
         }
-
-        dfsMemo[cur].put(from, result);
-        return result;
     }
 
-    private int count(int from, int cur, List<Integer>[] graph) {
-        if (countMemo[cur].containsKey(from))
-            return countMemo[cur].get(from);
+    private void countAndSum(Node root) {
+        root.sumBelow = 0;
+        root.count = 1;
 
-        int c = 1;
-
-        for (int nei : graph[cur]) {
-            if (nei != from)
-                c += count(cur, nei, graph);
+        for (Node c : root.children) {
+            countAndSum(c);
+            root.sumBelow += c.sumBelow + c.count;
+            root.count += c.count;
         }
+    }
 
-        countMemo[cur].put(from, c);
-        return c;
+    private void removeBackReferences(Node root) {
+        for (Node c : root.children) {
+            c.children.remove(root);
+            removeBackReferences(c);
+        }
     }
 }
