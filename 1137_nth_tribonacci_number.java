a0ed9b3c05ae0ab78472457c95dc5d32113a3e7c
farushed <farushed@tcd.ie>
2023-01-30 22:35:32 +0000

    1137

---
+++ 1137_nth_tribonacci_number.java
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/n-th-tribonacci-number/
+// Runtime 0 ms Beats 100%
+// Memory 38.8 MB Beats 95.74%
+
+class Solution {
+    public int tribonacci(int n) {
+        if (n <= 1)
+            return n;
+
+        int[] dp = new int[n+1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        for (int i=3; i<=n; i++) {
+            dp[i] = dp[i-3] + dp[i-2] + dp[i-1];
+        }
+
+        return dp[n];
+    }
+}
